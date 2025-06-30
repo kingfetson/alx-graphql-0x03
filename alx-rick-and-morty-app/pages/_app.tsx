ab8c44fec475572512012afd8e5c8d8313cee475
kingfetson <171272139+kingfetson@users.noreply.github.com>
2025-07-01 02:37:10 +0300

    Resolve merge conflicts

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+<<<<<<< HEAD
+import { ApolloProvider } from "@apollo/client";
+import client from "@/graphql/apolloClient";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+=======
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
+>>>>>>> 3d7ff773d87736bdf877bd7f2e77fa809f553ea0
 }
