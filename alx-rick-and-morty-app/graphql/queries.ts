92a57abd2519c30524fbd845e14acd1a16a833ff
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-07-01 10:30:35 +0300

    Update queries.ts
    created new content

--- alx-rick-and-morty-app/graphql/queries.ts
+++ alx-rick-and-morty-app/graphql/queries.ts
@@ -1,13 +1,13 @@
 import { gql } from "@apollo/client";
 
 export const GET_EPISODES = gql`
-  query getEpisodes($page: Int, $filter: FilterEpisode) {
-    episodes(page: $page, filter: $filter) {
+  query GetEpisodes($page: Int) {
+    episodes(page: $page) {
       info {
+        count
         pages
         next
         prev
-        count
       }
       results {
         id
